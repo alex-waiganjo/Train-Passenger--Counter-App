0a8f96cad819f5e600371d5aa7e4481f1b8d7bbc
alex-waiganjo <alexwaiganjo934@gmail.com>
2022-08-01 17:57:26 +0300

    upload files

---
+++ index.js
@@ -0,0 +1,23 @@
+function increment(){
+    let count = 0;
+   
+    
+    
+   
+    count = count + 1;
+    document.getElementById("count").innerHTML = count;
+
+}
+  
+
+
+//exercise
+let points = 50;
+points = points + 50
+console.log(points)
+
+points = points -75 
+console.log(points)
+
+points = points + 45 
+console.log(points)
